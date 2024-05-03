883d0e410cd0a001e869b1f5864a50e9613c3889
Avnish Kovi <avikovi@gmail.com>
2024-05-03 16:21:07 -0700

    Expose part 1

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,25 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornID = document.getElementById("horn-select");
+  const image = document.querySelector('img[alt="No image selected"]');
+  const audio = document.querySelector('audio');
+  hornSetup(hornID, image, audio);
+  playSound(audio);
+}
+
+function hornSetup(hornID, image, audio) {
+  hornID.addEventListener("change", function() {
+    const horn = hornID.value;
+    image.src = "assets/images/"+horn+".svg";
+    audio.src = "assets/audio/"+horn+".mp3";
+  })
+}
+
+function playSound(audio) {
+  var button = document.querySelector("button");
+  button.addEventListener("click", function() {
+    audio.load();
+    audio.play();
+  })
 }
