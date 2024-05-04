eea1338b79e2b7b7a55d2fe22df48a059b706f8a
Avnish Kovi <avikovi@gmail.com>
2024-05-03 17:17:18 -0700

    Added voice list

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,31 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const image = document.querySelector('img[alt="Smiling face"]');
+  function populateVoiceList() {
+    if (typeof speechSynthesis === "undefined") {
+      return;
+    }
+    const voices = speechSynthesis.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      document.getElementById("voice-select").appendChild(option);
+    }
+  }
+  populateVoiceList();
+  if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+  ) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
 }
